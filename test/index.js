a520ec4aa34b4955b144d4ba8f0c490264f5cfa9
1Computer1 <mineox100@gmail.com>
2017-07-07 20:16:29 -0400

    Modify tests to be more specific

--- test/index.js
+++ test/index.js
@@ -1,41 +1,45 @@
 const re = require('..');
 const assert = require('assert');
 
+function is(r1, r2) {
+    return r1.toString() === r2.toString();
+}
+
 const tests = {
     basic() {
         const regex = re`^Hello World$`;
-        return regex.test('Hello World');
+        return is(regex, /^Hello World$/);
     },
     flags() {
         const regex = re('i')`[a-z]`;
-        return regex.test('ABC');
+        return is(regex, /[a-z]/i);
     },
     escape() {
         const text = '?';
         const regex = re`wow${text}`;
-        return regex.test('wow?');
+        return is(regex, /wow\?/);
     },
     escapeIgnore() {
         const text = '?';
         const regex = re`wow${re.ignore(text)}`;
-        return regex.test('wo');
+        return is(regex, /wow?/);
     },
     noEscape() {
         const text = '?';
         const regex = re('', false)`wow${text}`;
-        return regex.test('wo');
+        return is(regex, /wow?/);
     },
     building() {
         const g = re('g');
         const digits = re.ignore`\d+`;
         const regex = g`-?${digits}`;
-        return regex.test('-123');
+        return is(regex, /-?\d+/g);
     },
     various() {
-        const one = '[-)*?{$';
+        const one = '[)*?{$';
         const two = 'a+b+c+';
-        const regex = re('i')`^escaped: ?${one} unescaped: ?${re.ignore(two)}$`;
-        return regex.test('ESCAPED:[-)*?{$ UNESCAPED:aaabbbccc');
+        const regex = re('i')`^${one} ?${re.ignore(two)}$`;
+        return is(regex, /^\[\)\*\?\{\$ ?a+b+c+$/i);
     }
 };
 
